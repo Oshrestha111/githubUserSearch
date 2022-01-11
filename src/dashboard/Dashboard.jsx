bfe53d458f3eba263b61ef4a7bd47cef4477d48c
Oriya Shrestha <oriya.shrestha111@gmail.com>
2022-01-11 20:09:40 +0545

    githubUserSearch

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./dashboard.css";
 import { FaPeriscope, FaSearch, FaTwitter } from "react-icons/fa";
 import { BsLink45Deg } from "react-icons/bs";
@@ -8,6 +8,27 @@ import octocat from "../img/Octocat.png";
 let iconStylse = { color: "RGB(20, 66, 132)", fontSize: "1em", padding: "5px" };
 let iconstyles = { fontSize: "14px" };
 function Dashboard() {
+	const [users, setUsers] = useState({});
+	const [username, setUsername] = useState("oshrestha111");
+
+	const getUsers = async () => {
+		const response = await fetch(`https://api.github.com/users/${username}`);
+		setUsers(await response.json());
+	};
+
+	const handleChange = (e) => {
+		setUsername(e.target.value);
+	};
+
+	const handleClick = () => {
+		getUsers();
+	};
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+	console.log(users);
+
 	return (
 		<div className='section'>
 			<div className='container'>
@@ -19,58 +40,62 @@ function Dashboard() {
 					<div className='search-bar'>
 						<FaSearch style={iconStylse} />
 						<input
+							onChange={handleChange}
+							value={username}
 							type='text'
 							placeholder='Search GitHub username . . . .'></input>
-						<button type='submit'>Search</button>
+						<button onClick={handleClick} on>
+							Search
+						</button>
 					</div>
 					<div className='desc'>
 						<div className='logo'>
-							<img src={octocat} />
+							<img src={users?.avatar_url} />
 						</div>
-						<div className='info'>
-							<div className='info-head'>
-								<h4>The Octocat</h4>
-								<label htmlFor=''> Joined 25 jan 2021</label>
+						<div className='right'>
+							<div className='info'>
+								<div className='info-head'>
+									<h4>{users?.login}</h4>
+									<label htmlFor=''>
+										{new Date(users?.created_at).toDateString()}
+									</label>
+								</div>
+								<div className='name'>{users?.name}</div>
 							</div>
-							<div className='name'>octocat</div>
-							<div className='bio'>This profile has no bio.</div>
-						</div>
-						<div></div>
-						<div className='count'>
-							<table>
-								<tr>
-									<th>Repos</th>
-									<th>Followers</th>
-									<th>Following</th>
-								</tr>
+							<div className='bio'>{users?.bio}</div>
 
-								<tr>
-									<td>8</td>
-									<td>3210</td>
-									<td>9</td>
-								</tr>
-							</table>
-						</div>
-						<div></div>
-						<div className='location'>
-							<table>
-								<tr>
-									<td>
-										<FaPeriscope style={iconstyles} /> San Francisco
-									</td>
-									<td>
-										<DiGit style={iconstyles} /> agithub
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<FaTwitter style={iconstyles} /> Not Available
-									</td>
-									<td>
-										<BsLink45Deg style={iconstyles} /> link
-									</td>
-								</tr>
-							</table>
+							<div className='count'>
+								<table>
+									<tr>
+										<th>Repos</th>
+										<th>Followers</th>
+										<th>Following</th>
+									</tr>
+
+									<tr>
+										<td>{users?.public_repos}</td>
+										<td>{users?.followers}</td>
+										<td>{users?.following}</td>
+									</tr>
+								</table>
+							</div>
+							<div></div>
+							<div className='location'>
+								<div className='grid'>
+									<span>
+										<FaPeriscope style={iconstyles} /> {users?.location}
+									</span>
+									<span>
+										<DiGit style={iconstyles} /> github
+									</span>
+									<span>
+										<FaTwitter style={iconstyles} /> {users?.twitter_username}
+									</span>
+									<span>
+										<BsLink45Deg style={iconstyles} /> {users?.blog}
+									</span>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
